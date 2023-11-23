c1d4ed811644fc414ddebe5a243ef6b25d7660e4
rautsab <rautsab@11gmail.com>
2023-11-23 10:24:59 +0600

    abc

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import {useFonts} from "expo-font";
 import {StatusBar} from "expo-status-bar";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import fonts from "./src/config/fonts";
+import fonts from "./frontend/src/config/fonts";
 
-import Navigation from "./src/navigation";
-import {AuthProvider} from "./src/context/AuthContext";
+import Navigation from "./frontend/src/navigation";
+import {AuthProvider} from "./frontend/src/context/AuthContext";
 
 export default function App() {
     const [fontsLoaded] = useFonts(fonts);
